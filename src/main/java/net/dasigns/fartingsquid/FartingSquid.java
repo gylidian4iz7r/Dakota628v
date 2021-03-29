18684866d881c0d4d6fca7e381cfb6f2c50a128f
gylidian4iz7r <wangxia9900@yeah.net>
2021-03-29 17:11:47 +0800

    Fully implemented farting squid

--- src/main/java/net/dasigns/fartingsquid/FartingSquid.java
+++ src/main/java/net/dasigns/fartingsquid/FartingSquid.java
@@ -1,10 +1,15 @@
 package net.dasigns.fartingsquid;
 
+import org.bukkit.Effect;
+import org.bukkit.EntityEffect;
 import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Squid;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.*;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 
@@ -22,7 +27,7 @@ public class FartingSquid extends JavaPlugin {
         getCommand("fartingsquid").setExecutor(new FartingSquidCommands(this));
 
         //Setup event listener
-        getServer().getPluginManager().registerEvents(new FartingSquidEvents(),this);
+        getServer().getPluginManager().registerEvents(new FartingSquidEvents(), this);
 
         this.getLogger().info("FartingSquid Enabled");
     }
@@ -33,13 +38,65 @@ public class FartingSquid extends JavaPlugin {
     }
 
     //Spawn a squid at the players location and mount the player on it
-    public void spawnSquid(Player p) {
+    public void spawnSquid(final Player p) {
         Location l = p.getLocation();
-        Squid s = (Squid) l.getWorld().spawnEntity(l, EntityType.SQUID);
+        final Squid s = (Squid) l.getWorld().spawnEntity(l, EntityType.SQUID);
         addFartingSquid(s);
         s.setMaxHealth(200);
         s.setHealth(200);
+        s.setMaximumAir(Integer.MAX_VALUE);
+        s.setRemainingAir(Integer.MAX_VALUE);
         s.setPassenger(p);
+
+        Runnable squidMove = new Runnable() {
+            public void run() {
+                if (s == null || s.getPassenger() == null || !(s.getPassenger() instanceof Player)) {
+                    if(getFartingSquids().contains(s)) removeFartingSquid(s);
+                    return;
+                }
+
+                //Explode blocks in our way
+                int blocksUp = 0;
+                Block target = s.getLocation().add(p.getLocation().getDirection()).getBlock();
+                while (blocksUp < 3) {
+                    if (target != null && target.getType() != Material.AIR) {
+
+                        s.getWorld().createExplosion(
+                                s.getLocation().getX(),
+                                s.getLocation().getY(),
+                                s.getLocation().getZ(),
+                                4,
+                                true,
+                                true
+                        );
+
+                        break;
+                    }
+                    target = target.getRelative(BlockFace.UP);
+                    blocksUp++;
+                }
+
+                //Clear ground items near us
+                for(Item i : s.getWorld().getEntitiesByClass(Item.class)) {
+                    if(i.getLocation().distance(s.getLocation()) <= 25) {
+                        i.remove();
+                    }
+                }
+
+                //Light fire to the block behind us...but there is probably nothing there
+                Block fire = s.getLocation().subtract(p.getLocation().getDirection()).getBlock();
+                fire.getRelative(BlockFace.UP).setType(Material.FIRE);
+
+                //Face the same direction as me squid!!
+                s.getLocation().setYaw(p.getLocation().getYaw());
+
+                //Continue moving
+                Vector velocity = p.getLocation().getDirection();
+                velocity.setY(0);
+                s.setVelocity(velocity);
+            }
+        };
+        getServer().getScheduler().runTaskTimer(this, squidMove, 1, 1);
     }
 
     public static FartingSquid getInstance() {
--- src/main/java/net/dasigns/fartingsquid/FartingSquidEvents.java
+++ src/main/java/net/dasigns/fartingsquid/FartingSquidEvents.java
@@ -1,14 +1,11 @@
 package net.dasigns.fartingsquid;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Squid;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.vehicle.VehicleEnterEvent;
 
 import java.util.ArrayList;
-import java.util.logging.Level;
 
 public class FartingSquidEvents implements Listener {
     private ArrayList<EntityDamageEvent.DamageCause> getSquidImmunites() {
@@ -19,12 +16,9 @@ public class FartingSquidEvents implements Listener {
         si.add(EntityDamageEvent.DamageCause.FIRE_TICK);
         si.add(EntityDamageEvent.DamageCause.SUFFOCATION);
         si.add(EntityDamageEvent.DamageCause.SUICIDE);
+        si.add(EntityDamageEvent.DamageCause.CONTACT);
+        si.add(EntityDamageEvent.DamageCause.LAVA);
         return si;
-    }
-
-    private boolean isFartingSquid(Entity e) {
-        if(!(e instanceof Squid)) return false;
-        return FartingSquid.getInstance().isFartingSquid((Squid) e);
     }
 
     @EventHandler //Don't take damage from explosions, fire, suicide or suffocation if squid or rider
@@ -43,14 +37,6 @@ public class FartingSquidEvents implements Listener {
             if(FartingSquid.getInstance().isFartingSquid(s) && getSquidImmunites().contains(e.getCause())) {
                 e.setCancelled(true);
             }
-        }
-    }
-
-    @EventHandler
-    public void vehicleEnterEvent(VehicleEnterEvent e) {
-        if(isFartingSquid(e.getVehicle())) {
-            FartingSquid.getInstance().getServer().getLogger().log(Level.INFO, "Now riding farting squid");
-            e.getVehicle().getWorld().createExplosion(e.getVehicle().getLocation(),10);
         }
     }
 }
